a2bc9e7190ca38209549e0173d37eefd5cce60ab
solidworkssa <solidworkssa@gmail.com>
2026-01-26 00:05:26 +0100

    feat: add app constants and configuration

---
+++ apps/web/lib/constants.ts
@@ -0,0 +1,6 @@
+export const BASE_CONTRACT_ADDRESS = process.env.NEXT_PUBLIC_BASE_CONTRACT_ADDRESS || ''
+export const STACKS_CONTRACT_ADDRESS = process.env.NEXT_PUBLIC_STACKS_CONTRACT_ADDRESS || ''
+export const STACKS_CONTRACT_NAME = process.env.NEXT_PUBLIC_STACKS_CONTRACT_NAME || 'witnesschain'
+
+export const BASE_CHAIN_ID = parseInt(process.env.NEXT_PUBLIC_BASE_CHAIN_ID || '8453')
+export const BASE_RPC_URL = process.env.NEXT_PUBLIC_BASE_RPC_URL || 'https://mainnet.base.org'
