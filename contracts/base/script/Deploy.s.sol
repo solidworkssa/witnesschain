dbf6aeb58b0eadf27d1152733f5b95294efee362
solidworkssa <solidworkssa@gmail.com>
2026-02-13 20:12:41 +0100

    ci(WitnessChain): deployment script

---
+++ contracts/base/script/Deploy.s.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+import "forge-std/Script.sol";
+import "../src/WitnessChain.sol";
+
+contract Deploy is Script {
+    function run() external {
+        vm.startBroadcast();
+        new WitnessChain();
+        vm.stopBroadcast();
+    }
+}
